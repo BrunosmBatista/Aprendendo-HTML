ce23e0b44169e147da832ea008e7b844c8aafa66
BrunosmBatista <brunosmbatista@gmail.com>
2021-08-05 10:39:38 -0300

    TelaLogin

---
+++ js/aprendendo.js
@@ -0,0 +1,48 @@
+    /*console.log("Teste....")
+
+    var i;
+
+    for(i = 0; i <=10; i ++){
+        console.log(i)
+        }
+    
+
+    var langs = ["html", "css", "JavaScript", "ovo"]
+    var localizacao = langs[0]
+    var tam = langs.length;
+    //console.log(langs)
+    langs[langs.length] = "PHP"
+    var novoTamanho  = langs.push("Java")
+    var last = langs.pop();
+    var first = langs.shift();
+    var novoTam = langs.unshift("React");
+    console.log(localizacao)
+    console.log(tam)
+    console.log(langs)
+    console.log(novoTamanho)
+    
+   var data1 = new Date();
+   console.log(data1)
+
+   var data2 = new Date("November 17, 2014 12:15:00");
+   console.log(data2)
+
+   var data3 = new Date(70767654324);
+   console.log(data3)
+
+   var data4 = new Date(2017, 10, 8, 14, 8, 28, 0);
+   console.log(data4)
+
+   var data5 = new Date(2014, 11, 8, 13, 10, 30, 0);
+   document.write(data5 + "<br />");
+
+   var data6 = new Date(2014, 11, 8, 13, 10, 30, 0);
+   document.write(data5.toUTCString() + "<br />");
+
+   var data7 = new Date(2014, 11, 8, 13, 10, 30, 0);
+   document.write(data5.toDateString());
+*/
+        var frase = "A linguagem Java e facil";
+        var pos1 = frase.indexOf("Java");
+
+        console.log(pos1);
